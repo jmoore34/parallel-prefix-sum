9452fe7e38bba27f4d806543d0cc0d025c5bf3fb
George Lu <georgelu418@gmail.com>
2020-02-26 14:17:56 -0600

    added <unistd.h> and tested fork()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <unistd.h>
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    int pid = fork();
+    if(pid==0){
+        cout << "child" << endl;
+    } else{
+        cout << "parent" << endl;
+    }
+    //command line argument
+
+    //input file
+
+    //validate inputs and number of elements
+
+    //output file
+
+    //create "large enough" shared memory space
+
+    //create child processes
+
     return 0;
 }
