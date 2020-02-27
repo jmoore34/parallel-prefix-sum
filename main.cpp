23c2f62863d75ed39a2827f36a5be10911f7acb5
Jonathan Moore <programjm@gmail.com>
2020-02-27 10:55:24 -0600

    Add command line option parsing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    // Input validation
+    if (argc != 5) { // 4 arguments + path
+        cout << "Error: Expected 4 arguments." << endl;
+        exit(1);
+    }
+
+    // Parse commandline arguments
+    int itemCount = stoi(argv[1]);
+    string inputFileName = argv[2];
+    string outputFileName = argv[3];
+    int numProcesses = stoi(argv[4]);
+
 }
